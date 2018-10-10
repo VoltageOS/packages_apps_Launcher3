715483a59bc11ae3a14545f0b809ab64afa6ae6f
Joey <joey@lineageos.org>
2018-10-10 08:27:37 +0200

    Launcher3: add toggle for desktop and drawer labels
    Co-authored-by: razorloves <razorloves@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I98063b7adaf22029c4bfa50d2cac730f3612e121
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -261,6 +261,8 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
                 DefaultsValueProvider.get(context).enableTwoLineToggle
             }
 
+        @JvmField val SHOW_DESKTOP_LABELS = backedUpItem("pref_desktop_show_labels", true)
+        @JvmField val SHOW_DRAWER_LABELS = backedUpItem("pref_drawer_show_labels", true)
         @JvmField val WORKSPACE_LOCK = backedUpItem("pref_workspace_lock", false)
         @JvmField val ALLAPPS_THEMED_ICONS = backedUpItem("pref_allapps_themed_icons", false)
         @JvmField
