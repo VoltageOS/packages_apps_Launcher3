4f35dce8bc18b5511ab28a3177698c2f0ecb38b1
Amir Zaidi <azaidi@live.nl>
2020-01-09 19:40:28 +0100

    Launcher3: Add AppInfo Bottom Sheet from Shade Launcher
    Shade: Rename ShortcutFactory to ShadeShortcutFactory
    
    Shade: Override AppInfoShortcut with BottomSheetShortcut
    
    Shade: Import bottom sheet class from Pie
    
    Shade: Fix ShadeShortcutFactory constructor
    
    Shade: Add testing entries to App Info
    
    Shade: Add real entries to App Info
    
    Shade: Implement parsing for app info
    
    Shade: Move all preference title strings to global strings file
    
    Shade: Set source to "Unknown" if it is empty
    
    Shade: Set icon pack to "None" by default
    
    Shade: Update app info bottom sheet to Q widgets bottom sheet
    
    Shade: Disable overscroll effect on bottom sheet preferences
    
    Shade: Update Version app info preference icon
    
    Shade: Reorder app info preference entries
    
    DennySPb: adapt to 11 and 12L without onClickListener proxy
    
    Change-Id: I34fe46944561723d3a47db92b3814e1deee80b5b
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -33,6 +33,7 @@ import android.os.RemoteException;
 import android.os.Process;
 import android.os.UserHandle;
 import android.util.Log;
+import android.view.InflateException;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -53,6 +54,7 @@ import com.android.launcher3.R;
 import com.android.launcher3.SecondaryDropTarget;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.allapps.PrivateProfileManager;
+import com.android.launcher3.customization.InfoBottomSheet;
 import com.android.launcher3.model.data.ItemInfo;
 import com.android.launcher3.model.data.WorkspaceItemInfo;
 import com.android.launcher3.pm.UserCache;
@@ -208,12 +210,22 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
 
         @Override
         public void onClick(View view) {
+            InfoBottomSheet cbs;
             Rect sourceBounds = Utilities.getViewBounds(view);
             ActivityOptionsWrapper options = mTarget.getActivityLaunchOptions(view, mItemInfo);
             // Dismiss the taskMenu when the app launch animation is complete
             options.onEndCallback.add(this::dismissTaskMenuView);
+            try {
+                cbs = (InfoBottomSheet) mTarget.getLayoutInflater().inflate(
+                        R.layout.app_info_bottom_sheet,
+                        mTarget.getDragLayer(),
+                        false);
+                cbs.configureBottomSheet(sourceBounds, view.getContext());
+                cbs.populateAndShow(mItemInfo);
+            } catch (InflateException e) {
             PackageManagerHelper.startDetailsActivityForInfo(view.getContext(), mItemInfo,
                     sourceBounds, options.toBundle());
+            }
             mTarget.getStatsLogManager().logger().withItemInfo(mItemInfo)
                     .log(LAUNCHER_SYSTEM_SHORTCUT_APP_INFO_TAP);
         }
