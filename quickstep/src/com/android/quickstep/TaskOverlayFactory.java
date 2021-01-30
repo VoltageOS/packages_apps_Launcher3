7691bac8308c62cd253b395abb81fea0c9da679d
Ali B <abittin@gmail.com>
2021-01-30 22:36:40 +0300

    Launcher3: Add uninstall button to system shortcuts
    [ghostrider-reborn: Adapt to Sv2, 13]
    
    Squashed:
    
        Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
        Date:   Sun Jun 26 18:44:18 2022 +0530
    
        Launcher3: SystemShortcut: Avoid NPE due to uninstall button
    
        Crash log: https://bin.kv2.dev/~62b85c7008bb984d9dc61e94
    
        Steps to reproduce:
        - Add Now Playing widget to home screen
        - Long press on it
        - Launcher crashes
    
        Test: manual; no longer crashes
        Change-Id: I6c0f73e38a3695a64d820c40b58f5e87ba7013c5
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Thu, 3 Feb 2022 22:14:38 +0530
        Subject: Launcher3: Use standard launcher method for uninstalling
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Tue, 24 Sep 2024 00:06:28 +0530
        Subject: Launcher3: Update UNINSTALL TaskShortcutFactory for A15
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    neobuddy89: Add back removed method to check system app and adapt.
    
    Change-Id: I005d676d9a98f65296c330e5e13fd0d849df6fe5
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -113,6 +113,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             TaskShortcutFactory.APP_INFO,
             TaskShortcutFactory.KILL_APP,
             TaskShortcutFactory.SPLIT_SELECT,
+            TaskShortcutFactory.UNINSTALL,
             TaskShortcutFactory.PIN,
             TaskShortcutFactory.INSTALL,
             TaskShortcutFactory.FREE_FORM,
