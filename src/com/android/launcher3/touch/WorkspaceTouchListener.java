2d362d1607dc64922454d167a83b5296db479862
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-04-28 10:34:14 +0700

    Use existing StatusBarTouchController to handle swipe down
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -47,8 +47,6 @@ import com.android.launcher3.views.OptionsPopupView;
 
 import com.android.launcher3.Utilities;
 
-import app.lawnchair.gestures.SwipeDownGesture;
-
 /**
  * Helper class to handle touch on empty space in workspace and show options popup on long press
  */
@@ -77,7 +75,6 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
     private final PowerManager mPm;
 
     private final GestureDetector mGestureDetector;
-    private final SwipeDownGesture mSwipeDown;
 
     public WorkspaceTouchListener(Launcher launcher, Workspace workspace) {
         mLauncher = launcher;
@@ -88,12 +85,10 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
         mPm = (PowerManager) workspace.getContext().getSystemService(Context.POWER_SERVICE);
         mGestureDetector = new GestureDetector(workspace.getContext(), this);
         mDoubleTapEnabled = Utilities.isDoubleTapGestureEnabled(workspace.getContext());
-        mSwipeDown = new SwipeDownGesture(workspace.getContext());
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent ev) {
-        mSwipeDown.onTouch(view, ev);
         mGestureDetector.onTouchEvent(ev);
         int action = ev.getActionMasked();
         if (action == ACTION_DOWN) {
