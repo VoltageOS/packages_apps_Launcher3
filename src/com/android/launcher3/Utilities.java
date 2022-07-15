ef35697eeea3d8dac5456d66c326cb0e1eeef941
Pranav Vashi <neobuddy89@gmail.com>
2022-07-15 17:47:04 +0530

    Launcher3: Add recents and app drawer opacity customization
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -158,6 +158,8 @@ public final class Utilities {
     public static final String SHOW_HOTSEAT_BG = "show_hotseat_bg";
     public static final String KEY_SHOW_SEARCH_BAR = "pref_show_search_bar";
     public static final String KEY_ALLOW_WALLPAPER_ZOOM = "pref_allow_wallpaper_zoom";
+    public static final String KEY_RECENTS_OPACITY = "pref_recents_opacity";
+    public static final String KEY_APP_DRAWER_OPACITY = "pref_app_drawer_opacity";
 
     /**
      * Returns true if theme is dark.
@@ -970,4 +972,13 @@ public final class Utilities {
         return prefs.getBoolean(KEY_ALLOW_WALLPAPER_ZOOM, false);
     }
 
+    public static int getRecentsOpacity(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_RECENTS_OPACITY, 80);
+    }
+
+    public static int getAllAppsOpacity(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_APP_DRAWER_OPACITY, 80);
+    }
 }
