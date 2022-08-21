a368a7b0d7d539d6936583a5523a9aba7728c0fc
LibXZR <i@xzr.moe>
2022-08-21 18:37:53 +0800

    Launcher3: Add support for parallel space
    spkal01: Forward Port to 13
    VOS: port to 14
    Change-Id: I13bee37cae645f50512f4d80cdfa25f84dce1b6b
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -93,6 +93,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
+import ink.kaleidoscope.ParallelSpaceManager;
 
 /**
  * All apps container view with search support for use in a dragging activity.
@@ -129,8 +130,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
 
     protected final T mActivityContext;
     protected final List<AdapterHolder> mAH;
-    protected final Predicate<ItemInfo> mPersonalMatcher = ItemInfoMatcher.ofUser(
-            Process.myUserHandle());
+    protected Predicate<ItemInfo> mPersonalMatcher;
     protected final WorkProfileManager mWorkManager;
     protected final Point mFastScrollerOffset = new Point();
     protected final int mScrimColor;
@@ -250,6 +250,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
                 new AlphabeticalAppsList<>(mActivityContext, mAllAppsStore, mWorkManager)));
         mAH.set(SEARCH, new AdapterHolder(SEARCH,
                 new AlphabeticalAppsList<>(mActivityContext, null, null)));
+        updateMatcher();
 
         getLayoutInflater().inflate(R.layout.all_apps_content, this);
         mHeader = findViewById(R.id.all_apps_header);
@@ -976,8 +977,16 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
         mBottomSheetAlpha = mActivityContext.getDeviceProfile().isTablet ? 1f : alpha;
     }
 
+    private void updateMatcher() {
+        mPersonalMatcher = ItemInfoMatcher.ofUser(
+                Process.myUserHandle()).or(ItemInfoMatcher.ofUsers(
+                    ParallelSpaceManager.getInstance().getParallelUserHandles()));
+        mWorkManager.updateMatcher();
+    }
+
     private void onAppsUpdated() {
         mHasWorkApps = Stream.of(mAllAppsStore.getApps()).anyMatch(mWorkManager.getMatcher());
+        updateMatcher();
         if (!isSearching()) {
             rebindAdapters();
             if (mHasWorkApps) {
--- src/com/android/launcher3/pm/UserCache.java
+++ src/com/android/launcher3/pm/UserCache.java
@@ -34,6 +34,8 @@ import com.android.launcher3.util.MainThreadInitializedObject;
 import com.android.launcher3.util.SafeCloseable;
 import com.android.launcher3.util.SimpleBroadcastReceiver;
 
+import ink.kaleidoscope.ParallelSpaceManager;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -81,12 +83,14 @@ public class UserCache implements SafeCloseable {
     @WorkerThread
     private void initAsync() {
         mUserChangeReceiver.register(mContext,
+                Context.RECEIVER_EXPORTED,
                 Intent.ACTION_MANAGED_PROFILE_AVAILABLE,
                 Intent.ACTION_MANAGED_PROFILE_UNAVAILABLE,
                 ACTION_PROFILE_ADDED,
                 ACTION_PROFILE_REMOVED,
                 ACTION_PROFILE_UNLOCKED,
-                ACTION_PROFILE_LOCKED);
+                ACTION_PROFILE_LOCKED,
+                Intent.ACTION_PARALLEL_SPACE_CHANGED);
         updateCache();
     }
 
@@ -146,6 +150,7 @@ public class UserCache implements SafeCloseable {
     private static Map<UserHandle, Long> queryAllUsers(UserManager userManager) {
         Map<UserHandle, Long> users = new ArrayMap<>();
         List<UserHandle> usersActual = userManager.getUserProfiles();
+        usersActual.addAll(ParallelSpaceManager.getInstance().getParallelUserHandles());
         if (usersActual != null) {
             for (UserHandle user : usersActual) {
                 long serial = userManager.getSerialNumberForUser(user);
