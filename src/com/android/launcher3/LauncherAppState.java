839d09248ab1a20c4c15325807fdb4d18ffa8069
elpaablo <donopwhile1@gmail.com>
2022-10-27 03:32:27 +0100

    Launcher3: Switch to AppLock API for protected apps
    @neobuddy89: Add MANAGE_APP_LOCK permission to privapp_whitelist.
    
    Change-Id: I519dafb9b9e8df683df532dcaf35c29acb7306e5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -54,9 +54,9 @@ import com.android.launcher3.icons.LauncherIconProvider;
 import com.android.launcher3.icons.LauncherIcons;
 import com.android.launcher3.logging.FileLog;
 import com.android.launcher3.lineage.trust.HiddenAppsFilter;
-import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 import com.android.launcher3.model.ModelLauncherCallbacks;
 import com.android.launcher3.model.WidgetsFilterDataProvider;
+import com.android.launcher3.lineage.trust.db.HiddenAppsDBHelper;
 import com.android.launcher3.notification.NotificationListener;
 import com.android.launcher3.pm.InstallSessionHelper;
 import com.android.launcher3.pm.InstallSessionTracker;
@@ -283,8 +283,8 @@ public class LauncherAppState implements SafeCloseable {
         return mIsSafeModeEnabled;
     }
 
-    public TrustDatabaseHelper getTrustData() {
-        return TrustDatabaseHelper.getInstance(this.mContext);
+    public HiddenAppsDBHelper getHiddenData() {
+        return HiddenAppsDBHelper.getInstance(this.mContext);
     }
 
     /**
