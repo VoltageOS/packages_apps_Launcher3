d87e5f4313a12f930cd3a44e3e941b032414c4ef
StarkDroid <gamerprince.exp@gmail.com>
2022-11-29 15:48:29 +0800

    Launcher3: Add toggle to show/hide app drawer search bar
    Change-Id: I0c03fde326700fe826f249f7c813034d27900b9f
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -187,6 +187,7 @@ public final class Utilities {
     public static final String KEY_RECENTS_OPACITY = "pref_recents_opacity";
     public static final String KEY_APP_DRAWER_OPACITY = "pref_app_drawer_opacity";
     public static final String KEY_BLUR_DEPTH = "pref_blur_depth";
+    public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
 
     /**
      * Returns true if theme is dark.
@@ -1103,5 +1104,10 @@ public final class Utilities {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getInt(KEY_BLUR_DEPTH,
                 (int) context.getResources().getDimension(R.dimen.max_depth_blur_radius));
+    }
+
+    public static boolean showSearch(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DRAWER_SEARCH, true);
     }
 }
