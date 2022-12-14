ce8616620bf996b3993e6122bb3a36be3fc7b2e7
Tommy Webb <tommy@calyxinstitute.org>
2022-12-14 16:13:06 -0500

    Launcher3: Use different badges for different work profiles
    * Supply the UserHandle to BitmapIcon, rather than FLAG_WORK, so that
      the proper badge icon can be used for a given profile (different
      colors).
    
    Change-Id: I64f1b425eee5cc7994616050411a881d84ece99d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -659,7 +659,10 @@ public final class Utilities {
 
         Drawable badge = null;
         if ((info instanceof ItemInfoWithIcon iiwi) && !iiwi.getMatchingLookupFlag().useLowRes()) {
-            badge = iiwi.bitmap.getBadgeDrawable(context, useTheme, getIconShapeOrNull(context));
+            try (LauncherIcons li = LauncherIcons.obtain(context)) {
+                badge = iiwi.bitmap.withUser(iiwi.user, li).getBadgeDrawable(context, useTheme,
+                        getIconShapeOrNull(context));
+            }
         }
 
         if (info instanceof PendingAddShortcutInfo) {
@@ -739,12 +742,14 @@ public final class Utilities {
         }
 
         if (badge == null) {
-            badge = BitmapInfo.LOW_RES_INFO.withFlags(
-                    UserCache.INSTANCE.get(context)
-                            .getUserInfo(info.user)
-                            .applyBitmapInfoFlags(FlagOp.NO_OP)
-                    )
-                    .getBadgeDrawable(context, useTheme, getIconShapeOrNull(context));
+            try (LauncherIcons li = LauncherIcons.obtain(context)) {
+                badge = BitmapInfo.LOW_RES_INFO.withUser(info.user, li).withFlags(
+                        UserCache.INSTANCE.get(context)
+                                .getUserInfo(info.user)
+                                .applyBitmapInfoFlags(FlagOp.NO_OP)
+                        )
+                        .getBadgeDrawable(context, useTheme, getIconShapeOrNull(context));
+            }
             if (badge == null) {
                 badge = new ColorDrawable(Color.TRANSPARENT);
             }
