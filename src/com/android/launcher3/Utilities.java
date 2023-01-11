d97cade238c386f0c9619c356d70aa57bdefdf9b
Dmitrii <bankersenator@gmail.com>
2023-01-11 07:06:36 +0000

    Launcher3: keep app drawer 100 opacity by default
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -926,6 +926,6 @@ public final class Utilities {
 
     public static int getAllAppsOpacity(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_APP_DRAWER_OPACITY, 80);
+        return prefs.getInt(KEY_APP_DRAWER_OPACITY, 100);
     }
 }
