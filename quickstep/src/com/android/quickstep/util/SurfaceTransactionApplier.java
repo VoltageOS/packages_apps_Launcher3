886cc413e8565f7aecfbd6b32ff45a74873a0b2d
Shen Lin <shhenlin@gmail.com>
2023-03-13 17:46:42 +0800

    Launcher3: Fix crash in SurfaceTransactionApplier
    Calling getViewRootImpl on a targetView is nullable, so we'd add a null
    check here before getting its SurfaceControl.
    
    Bug: 273143527
    Test: manual
    Change-Id: I245bc553ca32cf5b86978f826cfb2ca92def84d7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/util/SurfaceTransactionApplier.java
+++ quickstep/src/com/android/quickstep/util/SurfaceTransactionApplier.java
@@ -73,7 +73,8 @@ public class SurfaceTransactionApplier extends SurfaceReleaseCheck {
 
     private void initialize(View view) {
         mTargetViewRootImpl = view.getViewRootImpl();
-        mBarrierSurfaceControl = mTargetViewRootImpl.getSurfaceControl();
+        mBarrierSurfaceControl =
+                mTargetViewRootImpl != null ? mTargetViewRootImpl.getSurfaceControl() : null;
         mInitialized = true;
     }
 
