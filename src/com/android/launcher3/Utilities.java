7aedde2398e717082452a097d0716cf0266417ee
nift4 <nift4@protonmail.com>
2023-05-07 17:32:18 +0200

    Launcher3: Allow to control parallax effect and center wallpaper
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -181,6 +181,8 @@ public final class Utilities {
     public static final String KEY_HOTSEAT_OPACITY = "pref_hotseat_opacity";
     public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
     public static final String KEY_STATUS_BAR = "pref_show_statusbar";
+    public static final String KEY_SHORT_PARALLAX = "pref_short_parallax";
+    public static final String KEY_SINGLE_PAGE_CENTER = "pref_single_page_center";
 
     /**
      * Returns true if theme is dark.
@@ -1066,5 +1068,15 @@ public final class Utilities {
     public static boolean showStatusbarEnabled(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_STATUS_BAR, true);
+    }
+
+    public static boolean isShortParallax(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_SHORT_PARALLAX, false);
+    }
+
+    public static boolean isSinglePageCentered(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_SINGLE_PAGE_CENTER, false);
     }
 }
