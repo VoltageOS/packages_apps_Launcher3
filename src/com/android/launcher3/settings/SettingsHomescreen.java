c00264265c944e6a1670ac2fe06c76d486ee18cf
nift4 <nift4@protonmail.com>
2023-05-07 17:32:18 +0200

    Launcher3: Allow to control parallax effect and center wallpaper
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -109,7 +109,9 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (LauncherPrefs.SHOW_HOTSEAT_BG.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.HOTSEAT_OPACITY.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.SHOW_STATUS_BAR.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.SHOW_STATUS_BAR.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.SHORT_PARALLAX.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.SINGLE_PAGE_CENTER.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }
