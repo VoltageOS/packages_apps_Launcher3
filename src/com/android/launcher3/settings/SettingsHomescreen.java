412397d171fbdab5995c6ee53fcb513653b1582d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-11 09:01:52 +0800

    Launcher: Expose force monochrome themed icon flag as a user preference
    Change-Id: I81a58671ca5811ea18fb51c7689a0097eb103e87
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -117,6 +117,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             case Utilities.KEY_SHORT_PARALLAX:
             case Utilities.KEY_SINGLE_PAGE_CENTER:
             case Utilities.KEY_QUICKSPACE_SHOW_CITY:
+            case Utilities.KEY_FORCE_MONOCHROME_ICONS:
                 LauncherAppState.getInstanceNoCreate().setNeedsRestart();
                 break;
             default:
