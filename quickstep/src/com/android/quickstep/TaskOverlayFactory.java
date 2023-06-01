412e5c8687f09a41bd7db8b650a4aba1868d1034
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-01 22:20:40 +0530

    Launcher: Don't show uninstall shortcut in recents app menu
    Recents glitches out momentarily when an app is unstalled directly
    from its menu in recents. Having uninstall button there itself is
    bad UX in the first place, it's enough to have it in the app's long
    press menu in home screen/app drawer.
    
    Change-Id: I9c1796215ac62424a1de59e9ff88c801c5278c04
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -114,7 +114,6 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             TaskShortcutFactory.KILL_APP,
             TaskShortcutFactory.SPLIT_SELECT,
             TaskShortcutFactory.FLOATING,
-            TaskShortcutFactory.UNINSTALL,
             TaskShortcutFactory.PIN,
             TaskShortcutFactory.INSTALL,
             TaskShortcutFactory.FREE_FORM,
