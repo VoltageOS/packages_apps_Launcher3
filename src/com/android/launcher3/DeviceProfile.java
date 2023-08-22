713d799e08ab895f98d7b53d359133651954f931
Dhina17 <dhinalogu@gmail.com>
2023-08-22 14:45:56 +0530

    Launcher3: Calculate hotseat border space only if QSB available
    In landscape, hotseat border space happens to be very large
    so it breaks the hotseat icons view.
    Hence calculate hotseat border only if QSB present.
    (Also QSB won't be available in landscape)
    
    Change-Id: Iaefa0e1463c01cf05fa87153363ea9681f6e20fe
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -243,6 +243,8 @@ public class DeviceProfile {
     // not enough space, the hotseat will adjust itself for the bubble bar.
     private final int mBubbleBarSpaceThresholdPx;
 
+    private boolean mShowQsb;
+
     // Bottom sheets
     public int bottomSheetTopPadding;
     public int bottomSheetOpenDuration;
@@ -625,8 +627,8 @@ public class DeviceProfile {
         hotseatQsbVisualHeight = hotseatQsbHeight - 2 * hotseatQsbShadowHeight;
 
         // Whether QSB might be inline in appropriate orientation (e.g. landscape).
-        boolean showQsb = Utilities.showQSB(context);
-        boolean canQsbInline = showQsb && (isTwoPanels ? inv.inlineQsb[INDEX_TWO_PANEL_PORTRAIT]
+        mShowQsb = Utilities.showQSB(context);
+        boolean canQsbInline = mShowQsb && (isTwoPanels ? inv.inlineQsb[INDEX_TWO_PANEL_PORTRAIT]
                 || inv.inlineQsb[INDEX_TWO_PANEL_LANDSCAPE]
                 : inv.inlineQsb[INDEX_DEFAULT] || inv.inlineQsb[INDEX_LANDSCAPE])
                 && hotseatQsbHeight > 0;
@@ -655,7 +657,7 @@ public class DeviceProfile {
                             : hotseatSpecsProvider.getCalculatedSpec(responsiveAspectRatio,
                                     DimensionType.HEIGHT, heightPx);
             hotseatQsbSpace = mResponsiveHotseatSpec.getHotseatQsbSpace();
-            if (showQsb || isTaskbarPresent) {
+            if (mShowQsb || isTaskbarPresent) {
                 hotseatBarBottomSpace =
                     isVerticalBarLayout() ? 0 : mResponsiveHotseatSpec.getEdgePadding();
             } else {
@@ -672,9 +674,9 @@ public class DeviceProfile {
             mResponsiveWorkspaceCellSpec = workspaceCellSpecs.getCalculatedSpec(
                     responsiveAspectRatio, heightPx);
         } else {
-            hotseatQsbSpace = showQsb
+            hotseatQsbSpace = mShowQsb
                     ? pxFromDp(inv.hotseatQsbSpace[mTypeIndex], mMetrics) : 0;
-            hotseatBarBottomSpace = showQsb || isTaskbarPresent ? pxFromDp(
+            hotseatBarBottomSpace = mShowQsb || isTaskbarPresent ? pxFromDp(
                     inv.hotseatBarBottomSpace[mTypeIndex], mMetrics) : 0;
             mHotseatBarEdgePaddingPx =
                     isVerticalBarLayout() ? workspacePageIndicatorHeight : 0;
@@ -684,7 +686,7 @@ public class DeviceProfile {
 
         if (!isVerticalBarLayout()) {
             // Have a little space between the inset and the QSB
-            if (showQsb &&
+            if (mShowQsb &&
                     (mInsets.bottom + minQsbMargin) > hotseatBarBottomSpace) {
                 int availableSpace = hotseatQsbSpace - (mInsets.bottom - hotseatBarBottomSpace);
 
@@ -1415,9 +1417,11 @@ public class DeviceProfile {
      * This method calculates the space between the icons to achieve a certain width.
      */
     private int calculateHotseatBorderSpace(float hotseatWidthPx, int numExtraBorder) {
+        // Calculate hotseat border space only if QSB available.
+        if(!mShowQsb) return 0;
+
         int numBorders = (numShownHotseatIcons - 1 + numExtraBorder);
         if (numBorders <= 0) return 0;
-
         float hotseatIconsTotalPx = iconSizePx * numShownHotseatIcons;
         int hotseatBorderSpacePx = (int) (hotseatWidthPx - hotseatIconsTotalPx) / numBorders;
         return Math.min(hotseatBorderSpacePx, mMaxHotseatIconSpacePx);
