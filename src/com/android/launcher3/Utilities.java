6386e811155135045dedf58ae88bc3318574a313
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2024-04-09 11:21:54 +0000

    Launcher3: Restart on disabling overview action chips
    Change-Id: I045313b1828b8967701e87f9bbadb99d9e9aad50
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -193,6 +193,7 @@ public final class Utilities {
     public static final String KEY_HOTSEAT_QSB_OPACITY = "pref_hotseat_qsb_opacity";
     public static final String KEY_HOTSEAT_QSB_STROKE_WIDTH = "pref_hotseat_qsb_stroke_width";
     public static final String KEY_FORCE_MONOCHROME_ICON = "pref_force_monochrome_icon";
+    public static final String KEY_RECENTS_CHIPS = "pref_recents_chips";
 
     /**
      * Returns true if theme is dark.
