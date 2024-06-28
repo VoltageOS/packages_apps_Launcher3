b4e7cb08b986541b105cce7fbc7b78065a3bd7ca
Pranav Vashi <neobuddy89@gmail.com>
2024-06-29 01:26:12 +0530

    Launcher3: Add toggle to use two lines for app label
    Change-Id: I40c11b04995649611c215de3cf1955dd86a45f3f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -1363,8 +1363,7 @@ public class DeviceProfile {
         if (isVerticalLayout && !mIsResponsiveGrid) {
             hideWorkspaceLabelsIfNotEnoughSpace();
         }
-        if ((Flags.enableTwolineToggle()
-                && LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context))) {
+        if (LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context)) {
             if (!isEnglishLanguage(context)) {
                 // Set toggle preference value to false if not english here as it's possible the
                 // preference is stale after language change.
