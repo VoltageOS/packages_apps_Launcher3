41004189ab01e8ad4a5239b0a8d6d573ba6fb2bf
Pranav Vashi <neobuddy89@gmail.com>
2024-08-31 22:25:39 +0530

    Launcher3: Allow setting transparency for hotseat search bar
    Ref: https://github.com/LawnchairLauncher/lawnchair/commit/8244819d65c45f642685d24d58fd1397fe54e32e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -190,6 +190,7 @@ public final class Utilities {
     public static final String KEY_APP_DRAWER_OPACITY = "pref_app_drawer_opacity";
     public static final String KEY_BLUR_DEPTH = "pref_blur_depth";
     public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
+    public static final String KEY_HOTSEAT_QSB_OPACITY = "pref_hotseat_qsb_opacity";
 
     /**
      * Returns true if theme is dark.
@@ -1116,5 +1117,10 @@ public final class Utilities {
     public static boolean showSearch(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DRAWER_SEARCH, true);
+    }
+
+    public static int getHotseatQsbOpacity(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_HOTSEAT_QSB_OPACITY, 100);
     }
 }
