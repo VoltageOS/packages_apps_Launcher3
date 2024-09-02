31dc38204979aa2841d8b2988efac83e328a9951
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-09-02 13:43:26 +0300

    add Storage Scopes link to apps that have it enabled
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -282,6 +282,30 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
         protected abstract Intent getIntent(String targetPkg);
     }
 
+    public static final Factory<BaseActivity> STORAGE_SCOPES = StorageScopes::maybeGet;
+
+    public static class StorageScopes<T extends ActivityContext> extends ScopesShortcut<T> {
+
+        private StorageScopes(T target, ItemInfo itemInfo, View originalView) {
+            super(R.drawable.ic_sscopes_add_file, R.string.storage_scopes_drop_target_label, target,
+                    itemInfo, originalView);
+        }
+
+        @Nullable
+        public static <T extends ActivityContext> StorageScopes<T> maybeGet(T target, ItemInfo itemInfo, View originalView) {
+            if (hasGosPackageStateFlag(itemInfo, GosPackageState.FLAG_STORAGE_SCOPES_ENABLED)) {
+                return new StorageScopes<>(target, itemInfo, originalView);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected Intent getIntent(String targetPkg) {
+            return android.app.StorageScope.createConfigActivityIntent(targetPkg);
+        }
+    }
+
     public static final Factory<ActivityContext> PRIVATE_PROFILE_INSTALL =
             (context, itemInfo, originalView) -> {
                 if (originalView == null) {
