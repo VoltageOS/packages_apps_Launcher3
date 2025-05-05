NusantaraProject-ROM/android_packages_apps_Launcher3

Launcher: Extend notification listener with proxy
Change-Id: If6bb3d4a5c25deee8994044e0d99311d4ffad906

src/com/android/launcher3/Launcher.java
@@ -477,6 +477,7 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         setupViews();
         crossFadeWithPreviousAppearance();
         mPopupDataProvider = new PopupDataProvider(this::updateNotificationDots);
+        LauncherNotifications.getInstance().addListener(mPopupDataProvider);
 
         boolean internalStateHandled = ACTIVITY_TRACKER.handleCreate(this);
         if (internalStateHandled) {
@@ -987,7 +988,7 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         mModel.validateModelDataOnResume();
 
         // Set the notification listener and fetch updated notifications when we resume
-        NotificationListener.setNotificationsChangedListener(mPopupDataProvider);
+        NotificationListener.setNotificationsChangedListener(LauncherNotifications.getInstance());
 
         DiscoveryBounce.showForHomeIfNeeded(this);
         mAppWidgetHost.setActivityResumed(true);

VoltageOS/packages_apps_Launcher3

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/launcher3/Launcher.java
@@ -551,6 +551,7 @@ public class Launcher extends StatefulActivity<LauncherState>
         mPopupDataProvider = new PopupDataProvider(this::updateNotificationDots);
         mWidgetPickerDataProvider = new WidgetPickerDataProvider();
         PillColorProvider.getInstance(mWorkspace.getContext()).registerObserver();
+        LauncherNotifications.getInstance().addListener(mPopupDataProvider);
 
         boolean internalStateHandled = ACTIVITY_TRACKER.handleCreate(this);
         if (internalStateHandled) {
@@ -1151,7 +1152,7 @@ public class Launcher extends StatefulActivity<LauncherState>
         mModel.validateModelDataOnResume();
 
         // Set the notification listener and fetch updated notifications when we resume
-        NotificationListener.addNotificationsChangedListener(mPopupDataProvider);
+        NotificationListener.addNotificationsChangedListener(LauncherNotifications.getInstance());
 
         DiscoveryBounce.showForHomeIfNeeded(this);
         mAppWidgetHolder.setActivityResumed(true);