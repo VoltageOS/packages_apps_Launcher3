714de46f02c1f28aaf64136693093227a9dbb938
Pat Manning <patmanning@google.com>
2025-05-27 15:33:23 +0100

    Finish recents animation when dragging to launch non-live tile.
    Fix: 396255976
    Bug: 299060584
    Bug: 359117837
    Test: Manual.
    Flag: EXEMPT Bugfix
    Change-Id: I3ada253a88119cebc49c8f173bcc9347c1421bed
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -5957,7 +5957,8 @@ public abstract class RecentsView<
                     finishRecentsAnimation(false /* toRecents */, null);
                     onTaskLaunchAnimationEnd(true /* success */);
                 } else {
-                    taskView.launchWithoutAnimation(this::onTaskLaunchAnimationEnd);
+                    finishRecentsAnimation(true /* toRecents */,
+                            () -> taskView.launchWithoutAnimation(this::onTaskLaunchAnimationEnd));
                 }
                 mContainer.getStatsLogManager().logger().withItemInfo(taskView.getItemInfo())
                         .log(LAUNCHER_TASK_LAUNCH_SWIPE_DOWN);
