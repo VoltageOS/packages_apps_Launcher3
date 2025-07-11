a15ed309874d78c4f82ed03e1a5a3a353a03272f
Dmitrii <bankersenator@gmail.com>
2025-07-11 07:34:25 +0000

    Launcher3: drop all apps in the long press menu
    why?
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/views/OptionsPopupView.java
+++ src/com/android/launcher3/views/OptionsPopupView.java
@@ -220,11 +220,6 @@ public class OptionsPopupView<T extends Context & ActivityContext> extends Arrow
                     OptionsPopupView::enterHomeGardening));
         }
         options.add(new OptionItem(launcher,
-                R.string.all_apps_button_label,
-                R.drawable.ic_apps,
-                LAUNCHER_ALL_APPS_TAP_OR_LONGPRESS,
-                OptionsPopupView::enterAllApps));
-        options.add(new OptionItem(launcher,
                 R.string.styles_wallpaper_button_text,
                 R.drawable.ic_palette,
                 IGNORE,
