fd9690c3f9d6d75d516e2fc7941f4857ed0f2aaa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-08-12 23:24:07 +0530

    Launcher: Cancel ongoing animations before entering allapps
    This is required to avoid breaking background blur when opening app
    drawer simultaneously while closing an app, with the new workspace
    scaling animation.
    
    Change-Id: I4bea23ad7784f5ed9d45287e21058cc38ce51d86
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -154,6 +154,8 @@ import androidx.annotation.VisibleForTesting;
 import androidx.core.os.BuildCompat;
 import androidx.window.embedding.RuleController;
 
+import com.android.app.animation.Animations;
+
 import com.android.launcher3.DropTarget.DragObject;
 import com.android.launcher3.accessibility.LauncherAccessibilityDelegate;
 import com.android.launcher3.allapps.ActivityAllAppsContainerView;
@@ -1056,6 +1058,12 @@ public class Launcher extends StatefulActivity<LauncherState>
                                         .setPageIndex(getWorkspace().getCurrentPage())).build())
                         .log(getAllAppsEntryEvent().get());
             }
+        }
+        if (mPrevLauncherState == NORMAL) {
+            // Cancel any ongoing animations over workspace elements and hotseat in order to
+            // not conflict with workspace scaling animation.
+            Animations.Companion.cancelOngoingAnimation(getWorkspace());
+            Animations.Companion.cancelOngoingAnimation(getHotseat());
         }
         updateDisallowBack();
     }
