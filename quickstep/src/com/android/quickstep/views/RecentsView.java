d5f0a30dfa0387c621c02e3b4d99087b9eab8028
Pranav Vashi <neobuddy89@gmail.com>
2025-10-12 02:07:13 +0530

    Launcher3: RecentsView: Clear DepthController base surface on every exit
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -40,7 +40,6 @@ import static com.android.launcher3.Flags.enableDesktopExplodedView;
 import static com.android.launcher3.Flags.enableDesktopTaskAlphaAnimation;
 import static com.android.launcher3.Flags.enableGridOnlyOverview;
 import static com.android.launcher3.Flags.enableLargeDesktopWindowingTile;
-import static com.android.launcher3.Flags.enableOverviewBackgroundWallpaperBlur;
 import static com.android.launcher3.Flags.enableRefactorTaskThumbnail;
 import static com.android.launcher3.Flags.enableSeparateExternalDisplayTasks;
 import static com.android.launcher3.LauncherAnimUtils.SUCCESS_TRANSITION_PROGRESS;
@@ -1204,6 +1203,9 @@ public abstract class RecentsView<
     protected void onWindowVisibilityChanged(int visibility) {
         super.onWindowVisibilityChanged(visibility);
         updateTaskStackListenerState();
+        if (visibility != VISIBLE) {
+            mBlurUtils.setDrawLiveTileBelowRecents(false);
+        }
     }
 
     public void init(OverviewActionsView actionsView, SplitSelectStateController splitController,
@@ -6209,9 +6211,7 @@ public abstract class RecentsView<
         mRecentsAnimationController = null;
         mSplitSelectStateController.setRecentsAnimationRunning(false);
         executeSideTaskLaunchCallback();
-        if (enableOverviewBackgroundWallpaperBlur()) {
-            mBlurUtils.setDrawLiveTileBelowRecents(false);
-        }
+        mBlurUtils.setDrawLiveTileBelowRecents(false);
     }
 
     public void setDisallowScrollToClearAll(boolean disallowScrollToClearAll) {
