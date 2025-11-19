264bbb8eb25fd81d4e6ed0c310d3bdf213039819
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-19 21:03:28 +0800

    Launcher3: Removing blocking haptic to fix jank
    Change-Id: I5a81fdfe8d40d3578d43e2e39d1436e596a6cf12
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/allapps/AllAppsTransitionController.java
+++ src/com/android/launcher3/allapps/AllAppsTransitionController.java
@@ -408,19 +408,6 @@ public class AllAppsTransitionController
         builder.add(anim);
 
         setAlphas(toState, config, builder);
-        // This controls both haptics for tapping on QSB and going to all apps.
-        if (ALL_APPS.equals(toState) && mLauncher.isInState(NORMAL)) {
-            if (Flags.msdlFeedback()) {
-                if (config.isUserControlled()) {
-                    mMSDLPlayerWrapper.playToken(MSDLToken.SWIPE_THRESHOLD_INDICATOR);
-                } else {
-                    mMSDLPlayerWrapper.playToken(MSDLToken.TAP_HIGH_EMPHASIS);
-                }
-            } else {
-                mLauncher.getAppsView().performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY,
-                        HapticFeedbackConstants.FLAG_IGNORE_VIEW_SETTING);
-            }
-        }
     }
 
     public Animator createSpringAnimation(float... progressValues) {
