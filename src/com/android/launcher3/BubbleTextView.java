31b2f8340cc060498772aeb4bcd63000e0289b39
John Galt <johngaltfirstrun@gmail.com>
2026-02-25 14:42:10 -0500

    Launcher3: Optimize AllApps perf and icon allocations
    Only lookup adapter once per frame, enable LAYER_TYPE_HARDWARE, do not
    create new bitmap if already the same
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -1450,6 +1450,11 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         return mIsIconVisible ? mIcon : new ColorDrawable(Color.TRANSPARENT);
     }
 
+    @Override
+    public boolean hasOverlappingRendering() {
+        return false;
+    }
+
     /** Sets the icon visual state to disabled or not. */
     public void setIconDisabled(boolean isDisabled) {
         if (mIcon != null) {
@@ -1467,7 +1472,9 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         // same as before.
         mDisableRelayout = mIcon != null;
 
-        icon.setBounds(0, 0, mIconSize, mIconSize);
+        if (icon.getBounds().width() != mIconSize || icon.getBounds().height() != mIconSize) {
+            icon.setBounds(0, 0, mIconSize, mIconSize);
+        }
 
         updateIcon(icon);
 
@@ -1521,6 +1528,9 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         CacheLookupFlag expectedFlag = DEFAULT_LOOKUP_FLAG.withThemeIcon(shouldUseTheme());
         if (getTag() instanceof ItemInfoWithIcon info && !mHighResUpdateInProgress
                 && info.getMatchingLookupFlag().isVisuallyLessThan(expectedFlag)) {
+                if (mIcon != null && mIcon.isThemed() && shouldUseTheme()) {
+                    return;
+                }
             if (mIconLoadRequest != null) {
                 mIconLoadRequest.cancel();
             }
