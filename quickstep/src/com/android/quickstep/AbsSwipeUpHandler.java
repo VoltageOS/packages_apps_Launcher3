2941df0e321d0736b61f50e91274383e6ad829fe
Dmitrii <bankersenator@gmail.com>
2026-05-06 06:33:43 +0300

    Revert "Launcher3: Fix handling of input consumer when swiping recents"
    This reverts commit c91aaef620c236844dd179d019c0ee73152fff38.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -2430,9 +2430,11 @@ public abstract class AbsSwipeUpHandler<
     }
 
     private void invalidateHandler() {
+        if (!mContainerInterface.isInLiveTileMode() || mGestureState.getEndTarget() != RECENTS) {
+            mInputConsumerProxy.destroy();
+            mTaskAnimationManager.setLiveTileCleanUpHandler(null);
+        }
         mInputConsumerProxy.unregisterOnTouchDownCallback();
-        mInputConsumerProxy.destroy();
-        mTaskAnimationManager.setLiveTileCleanUpHandler(null);
         endRunningWindowAnim(false /* cancel */);
 
         if (mGestureEndCallback != null) {
@@ -2643,6 +2645,11 @@ public abstract class AbsSwipeUpHandler<
         }
         endLauncherTransitionController();
         mRecentsView.onSwipeUpAnimationSuccess();
+        mTaskAnimationManager.setLiveTileCleanUpHandler(() -> {
+            mRecentsView.cleanupRemoteTargets();
+            mInputConsumerProxy.destroy();
+        });
+        mTaskAnimationManager.enableLiveTileRestartListener();
 
         SystemUiProxy.INSTANCE.get(mContext).onOverviewShown(false, TAG);
         doLogGesture(RECENTS, mRecentsView.getCurrentPageTaskView());
