c116c4d158fb4c906a652331361cd036d57e3221
Dmitrii <bankersenator@gmail.com>
2026-07-27 08:41:12 +0000

    Revert "Launcher: Force update statusbar color on allapps"
    This reverts commit 5d5f5154e51f0081cb721fec0b20e6d9dcc5b03e.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/allapps/AllAppsTransitionController.java
+++ src/com/android/launcher3/allapps/AllAppsTransitionController.java
@@ -30,9 +30,7 @@ import static com.android.launcher3.anim.PropertySetter.NO_ANIM_PROPERTY_SETTER;
 import static com.android.launcher3.states.StateAnimationConfig.ANIM_ALL_APPS_FADE;
 import static com.android.launcher3.states.StateAnimationConfig.ANIM_VERTICAL_PROGRESS;
 import static com.android.launcher3.util.SystemUiController.FLAG_DARK_NAV;
-import static com.android.launcher3.util.SystemUiController.FLAG_DARK_STATUS;
 import static com.android.launcher3.util.SystemUiController.FLAG_LIGHT_NAV;
-import static com.android.launcher3.util.SystemUiController.FLAG_LIGHT_STATUS;
 import static com.android.launcher3.util.SystemUiController.UI_STATE_ALL_APPS;
 
 import android.animation.Animator;
@@ -86,7 +84,6 @@ public class AllAppsTransitionController
     public static final int REVERT_SWIPE_ALL_APPS_TO_HOME_ANIMATION_DURATION_MS = 200;
 
     private static final float NAV_BAR_COLOR_FORCE_UPDATE_THRESHOLD = 0.1f;
-    private static final float STATUS_BAR_COLOR_FORCE_UPDATE_THRESHOLD = 0.4f;
 
     public static final FloatProperty<AllAppsTransitionController> ALL_APPS_PROGRESS =
             new FloatProperty<>("allAppsProgress") {
@@ -108,7 +105,7 @@ public class AllAppsTransitionController
 
     private final Launcher mLauncher;
     private final AnimatedFloat mAllAppScale = new AnimatedFloat(this::onScaleProgressChanged);
-    private final int mNavScrimFlag, mStatusScrimFlag;
+    private final int mNavScrimFlag;
 
     @Nullable private Animator.AnimatorListener mAllAppsSearchBackAnimationListener;
 
@@ -140,8 +137,6 @@ public class AllAppsTransitionController
         mIsVerticalLayout = dp.isVerticalBarLayout();
         mNavScrimFlag = Themes.getAttrBoolean(l, R.attr.isMainColorDark)
                 ? FLAG_DARK_NAV : FLAG_LIGHT_NAV;
-        mStatusScrimFlag = Themes.getAttrBoolean(l, R.attr.isMainColorDark)
-                ? FLAG_DARK_STATUS : FLAG_LIGHT_STATUS;
 
         setShiftRange(dp.getAllAppsProfile().getShiftRange());
         mAllAppScale.value = 1;
@@ -184,11 +179,10 @@ public class AllAppsTransitionController
         getAppsViewProgressTranslationY().setValue(mProgress * shiftRange);
         mLauncher.onAllAppsTransition(1 - progress);
 
-        boolean hasNavScrim = progress < NAV_BAR_COLOR_FORCE_UPDATE_THRESHOLD
+        boolean hasScrim = progress < NAV_BAR_COLOR_FORCE_UPDATE_THRESHOLD
                 && mLauncher.getAppsView().getNavBarScrimHeight() > 0;
-        boolean hasStatusScrim = progress < STATUS_BAR_COLOR_FORCE_UPDATE_THRESHOLD;
-        mLauncher.getSystemUiController().updateUiState(UI_STATE_ALL_APPS,
-                ((hasNavScrim ? mNavScrimFlag : 0) | (hasStatusScrim ? mStatusScrimFlag : 0)));
+        mLauncher.getSystemUiController().updateUiState(
+                UI_STATE_ALL_APPS, hasScrim ? mNavScrimFlag : 0);
     }
 
     public float getProgress() {
