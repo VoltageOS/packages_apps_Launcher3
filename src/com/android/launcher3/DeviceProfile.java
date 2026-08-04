GrapheneOS/platform_packages_apps_Launcher3

fixup! Launcher3: Add pref to set app drawer row height
Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/launcher3/DeviceProfile.java
@@ -909,14 +909,13 @@ public class DeviceProfile {
             mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(cellHeight);
         }
 
-        if (LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context)
-                && allAppsIconText
-                && !(mIsResponsiveGrid && getAllAppsProfile().getMaxAllAppsTextLineCount() == 2)) {
-            // Add extra textHeight to the existing allAppsCellHeight.
-            mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(
-                    (int) ((float) getAllAppsProfile().getCellHeightPx() * allAppsCellHeightMultiplier)
-                    + Utilities.calculateTextHeight( getAllAppsProfile().getIconTextSizePx())
-            );
+        if (allAppsIconText) {
+            int cellHeight = (int) (getAllAppsProfile().getCellHeightPx() * allAppsCellHeightMultiplier);
+            if (LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context)
+                    && !(mIsResponsiveGrid && getAllAppsProfile().getMaxAllAppsTextLineCount() == 2)) {
+                cellHeight += Utilities.calculateTextHeight(getAllAppsProfile().getIconTextSizePx());
+            }
+            mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(cellHeight);
         }
 
         updateHotseatSizes(getWorkspaceIconProfile().getIconSizePx());

VoltageOS/packages_apps_Launcher3

src/com/android/launcher3/DeviceProfile.java
@@ -448,15 +448,13 @@ public class DeviceProfile {
             mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(cellHeight);
         }
 
-        if (LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context)
-                && mAllAppsIconText
-                && !(mIsResponsiveGrid && getAllAppsProfile().getMaxAllAppsTextLineCount() == 2)) {
-            // Add extra textHeight to the existing allAppsCellHeight.
-            mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(
-                    (int) (getAllAppsProfile().getCellHeightPx()
-                            * mAllAppsCellHeightMultiplier)
-                            + Utilities.calculateTextHeight(getAllAppsProfile().getIconTextSizePx())
-            );
+        if (mAllAppsIconText) {
+            int cellHeight = (int) (getAllAppsProfile().getCellHeightPx() * mAllAppsCellHeightMultiplier);
+            if (LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context)
+                    && !(mIsResponsiveGrid && getAllAppsProfile().getMaxAllAppsTextLineCount() == 2)) {
+                cellHeight += Utilities.calculateTextHeight(getAllAppsProfile().getIconTextSizePx());
+            }
+            mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(cellHeight);
         }
 
         mBottomSheetProfile = BottomSheetProfile.Factory.createBottomSheetProfile(